9f9384c990fef30a926073238a38ad3ebf30b1c3
Alexander I.Grafov <grafov@gmail.com>
2016-04-17 22:12:02 +0300

    Add unit test for unmarhal board with annotation.

--- client/dashboard-unmarshal_test.go
+++ client/dashboard-unmarshal_test.go
@@ -30,6 +30,17 @@ func TestUnmarshal_EmptyDashboardWithTemplating26(t *testing.T) {
 	}
 }
 
+func TestUnmarshal_EmptyDashboardWithAnnotation26(t *testing.T) {
+	var board grafana.Board
+	raw, _ := ioutil.ReadFile("../testdata/empty-dashboard-with-annotation-2.6.json")
+
+	err := json.Unmarshal(raw, &board)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
 func TestUnmarshal_DashboardWithDefaultPanelsIn2Rows26(t *testing.T) {
 	var board grafana.Board
 	raw, _ := ioutil.ReadFile("../testdata/default-panels-all-types-2-rows-dashboard-2.6.json")
