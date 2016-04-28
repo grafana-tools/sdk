495438e3556e6f6ff10a28f39a0c1368df4ee7c7
Alexander I.Grafov <grafov@gmail.com>
2016-04-28 08:08:15 +0300

    Add missed file with an example.

---
+++ cmd/backup-datasources/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/gosimple/slug"
+	"github.com/grafov/autograf/client"
+	"github.com/grafov/autograf/grafana"
+)
+
+/* This is a simple example of usage of Grafana client
+for copying datasources and saving them to a disk.
+It is useful for Grafana backups!
+
+Usage:
+  backup-datasources http://grafana.host:3000 api-key-string-here
+
+You need get API key with Admin rights from your Grafana!
+*/
+
+func main() {
+	var (
+		datasources []grafana.Datasource
+		dsPacked    []byte
+		meta        client.BoardProperties
+		err         error
+	)
+	if len(os.Args) != 3 {
+		fmt.Fprint(os.Stderr, "Usage:  backup-datasources http://grafana.host:3000 api-key-string-here\n")
+		os.Exit(0)
+	}
+	c := client.New(os.Args[1], os.Args[2])
+	if datasources, err = c.GetAllDatasources(); err != nil {
+		fmt.Fprintf(os.Stderr, fmt.Sprintf("%s\n", err))
+		os.Exit(1)
+	}
+	for _, ds := range datasources {
+		if dsPacked, err = json.Marshal(ds); err != nil {
+			fmt.Fprintf(os.Stderr, fmt.Sprintf("%s for %s\n", err, ds.Name))
+			continue
+		}
+		if err = ioutil.WriteFile(fmt.Sprintf("%s.json", slug.Make(ds.Name)), dsPacked, os.FileMode(int(0666))); err != nil {
+			fmt.Fprintf(os.Stderr, fmt.Sprintf("%s for %s\n", err, meta.Slug))
+		}
+	}
+}
