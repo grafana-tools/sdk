2f66e8bb60b54886a586f299c20e918360a3fd0d
Alexander I.Grafov <grafov@gmail.com>
2016-06-29 16:17:22 +0300

    Add unit test for Elasticsearch datasource parsing

--- grafana/panel_test.go
+++ grafana/panel_test.go
@@ -185,3 +185,116 @@ func TestGraph_SetTargetUpdate(t *testing.T) {
 		t.Errorf("should be equal A but %s", graph.GraphPanel.Targets[0].RefID)
 	}
 }
+
+// Test on the panel sample with Elasticsearch datasource with Graylog query. Grafana 2.6.
+func TastPanel_ElasticsearchSource_ParsedTargets(t *testing.T) {
+	var rawPanel = []byte(`{
+  "aliasColors": {},
+  "bars": true,
+  "datasource": "Example GrayLog",
+  "editable": true,
+  "error": false,
+  "fill": 1,
+  "grid": {
+    "leftLogBase": 1,
+    "leftMax": null,
+    "leftMin": null,
+    "rightLogBase": 1,
+    "rightMax": null,
+    "rightMin": null,
+    "threshold1": null,
+    "threshold1Color": "rgba(216, 200, 27, 0.27)",
+    "threshold2": null,
+    "threshold2Color": "rgba(234, 112, 112, 0.22)"
+  },
+  "id": 37,
+  "isNew": true,
+  "legend": {
+    "avg": false,
+    "current": false,
+    "max": false,
+    "min": false,
+    "show": false,
+    "total": false,
+    "values": false
+  },
+  "lines": false,
+  "linewidth": 2,
+  "links": [
+    {
+      "params": "q=tag%3A%2Fid.*%2F+AND+level%3AERROR&rangetype=relative&relative=300#fields=message%2Csource",
+      "title": "Example GrayLog Page",
+      "type": "absolute",
+      "url": "https://graylog/streams/xxx/messages"
+    }
+  ],
+  "nullPointMode": "connected",
+  "percentage": false,
+  "pointradius": 5,
+  "points": false,
+  "renderer": "flot",
+  "seriesOverrides": [],
+  "span": 2,
+  "stack": false,
+  "steppedLine": false,
+  "targets": [
+    {
+      "bucketAggs": [
+        {
+          "field": "timestamp",
+          "id": "2",
+          "settings": {
+            "interval": "5m",
+            "min_doc_count": 0
+          },
+          "type": "date_histogram"
+        }
+      ],
+      "dsType": "elasticsearch",
+      "metrics": [
+        {
+          "field": "select field",
+          "id": "1",
+          "type": "count"
+        }
+      ],
+      "query": "tag:/.*.xxx.filtered/ AND tag:/id.*/ AND level:ERROR",
+      "refId": "A",
+      "target": "",
+      "timeField": "timestamp"
+    }
+  ],
+  "timeFrom": null,
+  "timeShift": null,
+  "title": "Example GrayLog Errors[5m]",
+  "tooltip": {
+    "shared": true,
+    "value_type": "cumulative"
+  },
+  "transparent": true,
+  "type": "graph",
+  "x-axis": true,
+  "y-axis": true,
+  "y_formats": [
+    "short",
+    "short"
+  ]
+}`)
+
+	var graph *Panel
+	err := json.Unmarshal(rawPanel, graph)
+
+	if err != nil {
+		t.Error(err)
+	}
+	if len(graph.GraphPanel.Targets) != 1 {
+		t.Errorf("should be 1 but %d", len(graph.GraphPanel.Targets))
+	}
+	if graph.GraphPanel.Targets[0].DsType == nil {
+		t.Fatalf("should be \"elasticsearch\" but nil")
+	}
+	if *graph.GraphPanel.Targets[0].DsType != "elasticsearch" {
+		t.Errorf("should be \"elasticsearch\" but %s", graph.GraphPanel.Targets[0].DsType)
+	}
+
+}
