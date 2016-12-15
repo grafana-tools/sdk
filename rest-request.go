7280a7e545b72077c512223bb9296e23e783efa7
Alexander I.Grafov <grafov@gmail.com>
2016-12-15 05:22:01 +0300

    Rename for better naming in a single lib.

--- rest-request.go
+++ rest-request.go
@@ -30,13 +30,14 @@ import (
 // It allows you globally redefine HTTP client.
 var DefaultHTTPClient = http.DefaultClient
 
-// Instance of Grafana.
-type Instance struct {
+// Client uses Grafana REST API for interacting with Grafana server.
+type Client struct {
 	baseURL string
 	key     string
 	client  *http.Client
 }
 
+// StatusMessage reflects status message as it returned by Grafana REST API.
 type StatusMessage struct {
 	ID      *uint   `json:"id"`
 	Message *string `json:"message"`
@@ -45,12 +46,12 @@ type StatusMessage struct {
 	Status  *string `json:"resp"`
 }
 
-// New keeps request data.
-func New(apiURL, apiKey string, client *http.Client) *Instance {
-	return &Instance{baseURL: apiURL, key: fmt.Sprintf("Bearer %s", apiKey), client: client}
+// NewClient initializes client for interacting with an instance of Grafana server.
+func NewClient(apiURL, apiKey string, client *http.Client) *Client {
+	return &Client{baseURL: apiURL, key: fmt.Sprintf("Bearer %s", apiKey), client: client}
 }
 
-func (r *Instance) get(query string, params url.Values) ([]byte, int, error) {
+func (r *Client) get(query string, params url.Values) ([]byte, int, error) {
 	u, _ := url.Parse(r.baseURL)
 	u.Path = query
 	if params != nil {
@@ -69,7 +70,7 @@ func (r *Instance) get(query string, params url.Values) ([]byte, int, error) {
 	return data, resp.StatusCode, err
 }
 
-func (r *Instance) put(query string, params url.Values, body []byte) ([]byte, int, error) {
+func (r *Client) put(query string, params url.Values, body []byte) ([]byte, int, error) {
 	u, _ := url.Parse(r.baseURL)
 	u.Path = query
 	if params != nil {
@@ -91,7 +92,7 @@ func (r *Instance) put(query string, params url.Values, body []byte) ([]byte, in
 	return data, resp.StatusCode, err
 }
 
-func (r *Instance) post(query string, params url.Values, body []byte) ([]byte, int, error) {
+func (r *Client) post(query string, params url.Values, body []byte) ([]byte, int, error) {
 	u, _ := url.Parse(r.baseURL)
 	u.Path = query
 	if params != nil {
@@ -111,7 +112,7 @@ func (r *Instance) post(query string, params url.Values, body []byte) ([]byte, i
 	return data, resp.StatusCode, err
 }
 
-func (r *Instance) delete(query string) ([]byte, error) {
+func (r *Client) delete(query string) ([]byte, error) {
 	u, _ := url.Parse(r.baseURL)
 	u.Path = query
 	req, err := http.NewRequest("DELETE", u.String(), nil)
