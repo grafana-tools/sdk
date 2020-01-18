1005e40fdc83130c42227fe7685cc4d10bc8b40a
Giedrius Statkevičius <giedriuswork@gmail.com>
2020-01-18 21:10:10 +0200

    alertnotification: initial attempt at integration tests

---
+++ rest-alertnotification_integration_test.go
@@ -0,0 +1,18 @@
+package sdk
+
+import (
+	"http"
+	"testing"
+)
+
+var client = NewClient("http://localhost:3000", "admin:admin", DefaultClient)
+
+func Test_Alertnotification_CRUD(t *testing.T) {
+	alertnotifications, err := client.GetAllAlertNotifications()
+	if err != nil {
+		t.Error(err)
+	}
+	if len(alertnotifications) != 0 {
+		t.Errorf("expected to get zero alertnotifications, got %#v", alertnotifications)
+	}
+}
