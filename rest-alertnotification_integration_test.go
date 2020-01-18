ebd86a6bef520cba85ef09044c8039fa12a2f246
Giedrius Statkevičius <giedriuswork@gmail.com>
2020-01-18 21:23:43 +0200

    integration: alertnotification: add more

--- rest-alertnotification_integration_test.go
+++ rest-alertnotification_integration_test.go
@@ -1,17 +1,52 @@
 package sdk
 
 import (
+	"os"
 	"testing"
 )
 
 var client = NewClient("http://localhost:3000", "admin:admin", DefaultHTTPClient)
 
 func Test_Alertnotification_CRUD(t *testing.T) {
+	if v := os.Getenv("GRAFANA_INTEGRATION"); v != "1" {
+		t.Skip("skipping because GRAFANA_INTEGRATION is %s, not 1", v)
+	}
+
 	alertnotifications, err := client.GetAllAlertNotifications()
 	if err != nil {
 		t.Error(err)
 	}
 	if len(alertnotifications) != 0 {
 		t.Errorf("expected to get zero alertnotifications, got %#v", alertnotifications)
+	}
+
+	an := AlertNotification{
+		Name:                  "alertnotification",
+		Type:                  "prometheus",
+		IsDefault:             true,
+		DisableResolveMessage: true,
+		SendReminder:          true,
+		Frequency:             "123",
+		UID:                   "foobar",
+	}
+
+	id, err := client.CreateAlertNotification(an)
+	if err != nil {
+		t.Error(err)
+	}
+
+	anRetrieved, err := client.GetAlertNotificationID(uint(id))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if anRetrieved.Name != an.Name {
+		t.Errorf("got wrong name: expected %s, was %s", anRetrieved.Name, an.Name)
+	}
+
+	an.Name = "alertnotification2"
+	err := client.UpdateAlertNotificationUID(an, "foobar")
+	if err != nil {
+		t.Error(err)
 	}
 }
