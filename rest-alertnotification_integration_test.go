31887258fdb4bfed58bbaa727855a0ade32eb5c5
Giedrius Statkevičius <giedriuswork@gmail.com>
2020-01-20 21:15:26 +0200

    alertnotification: initial attempt at integration tests (#57)
    * alertnotification: initial attempt at integration tests
    
    * integration: alertnotification: fix import
    
    * integration: alertnotification: fix
    
    * integration: alertnotification: add more
    
    * integration: alertnotification: add more CRUD OPs
    
    * integration: alertnotification: fix linter problem
    
    * integration: alertnotification: add Settings
    
    * integration: alertnotification: copy/paste example from Grafana docs

---
+++ rest-alertnotification_integration_test.go
@@ -0,0 +1,65 @@
+package sdk
+
+import (
+	"os"
+	"testing"
+)
+
+var client = NewClient("http://localhost:3000", "admin:admin", DefaultHTTPClient)
+
+func Test_Alertnotification_CRUD(t *testing.T) {
+	if v := os.Getenv("GRAFANA_INTEGRATION"); v != "1" {
+		t.Skipf("skipping because GRAFANA_INTEGRATION is %s, not 1", v)
+	}
+
+	alertnotifications, err := client.GetAllAlertNotifications()
+	if err != nil {
+		t.Error(err)
+	}
+	if len(alertnotifications) != 0 {
+		t.Errorf("expected to get zero alertnotifications, got %#v", alertnotifications)
+	}
+
+	an := AlertNotification{
+		Name:                  "team-a-email-notifier",
+		Type:                  "email",
+		IsDefault:             false,
+		DisableResolveMessage: false,
+		SendReminder:          false,
+		Frequency:             "15m",
+		UID:                   "foobar",
+		Settings: map[string]string{
+			"addresses": "dev@null.com",
+		},
+	}
+
+	id, err := client.CreateAlertNotification(an)
+	if err != nil {
+		t.Error(err)
+	}
+
+	anRetrieved, err := client.GetAlertNotificationID(uint(id))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if anRetrieved.Name != an.Name {
+		t.Errorf("got wrong name: expected %s, was %s", anRetrieved.Name, an.Name)
+	}
+
+	an.Name = "alertnotification2"
+	err = client.UpdateAlertNotificationUID(an, "foobar")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = client.DeleteAlertNotificationUID("foobar")
+	if err != nil {
+		t.Error(err)
+	}
+
+	an, err = client.GetAlertNotificationUID("foobar")
+	if err == nil {
+		t.Errorf("expected the alertnotification to be deleted")
+	}
+}
