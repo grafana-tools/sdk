0be3d7d09bf84458e87c3a9001695d14b19306b3
Giedrius Statkevičius <giedriuswork@gmail.com>
2020-01-25 19:26:55 +0200

    integration: add admin tests, make things stricter (#59)
    Use t.Fail{,f}() everywhere now to fail immediately once something goes
    wrong because the tests are written in a way that we cannot continue if
    something goes wrong.
    
    Add initial integration tests for admin REST API calls.
    
    Signed-off-by: Giedrius Statkevičius <giedriuswork@gmail.com>

---
+++ rest-admin_integration_test.go
@@ -0,0 +1,52 @@
+package sdk_test
+
+import (
+	"github.com/grafana-tools/sdk"
+	"testing"
+)
+
+func TestAdminOperations(t *testing.T) {
+	shouldSkip(t)
+	client := getClient()
+
+	u := sdk.User{
+		Login:          "test",
+		Name:           "name",
+		Email:          "grafana@sdk.com",
+		Theme:          "",
+		Password:       "vvvvvvvvvvvv",
+		IsGrafanaAdmin: false,
+	}
+
+	st, err := client.CreateUser(u)
+	if err != nil {
+		t.Fatalf("failed to create an user: %s", err.Error())
+	}
+	if st.Message != nil && *st.Message == "failed to create user" {
+		t.Fatal("failed to create an user for some unknown reason")
+	}
+
+	uid := *st.ID
+
+	retrievedUser, err := client.GetUser(uid)
+	if err != nil {
+		t.Fatalf("failed to get user: %s", err.Error())
+	}
+
+	if retrievedUser.Login != u.Login || retrievedUser.Email != u.Email {
+		t.Fatal("retrieved data does not match what was created")
+	}
+
+	_, err = client.UpdateUserPermissions(sdk.UserPermissions{IsGrafanaAdmin: true}, uid)
+	if err != nil {
+		t.Fatalf("failed to convert the user into an admin: %s", err)
+	}
+
+	retrievedUser, err = client.GetUser(uid)
+	if err != nil {
+		t.Fatalf("failed to get user: %s", err.Error())
+	}
+	if retrievedUser.IsGrafanaAdmin != true {
+		t.Fatal("user should be an admin but is not")
+	}
+}
