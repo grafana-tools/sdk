ef67590058e8e36d3f85dfb3e124a054b2beb036
Xabier Larrakoetxea <me@slok.dev>
2020-04-03 08:10:16 +0200

    Refactor dashboard CRUD integration tests
    Signed-off-by: Xabier Larrakoetxea <me@slok.dev>

--- rest-dashboard_integration_test.go
+++ rest-dashboard_integration_test.go
@@ -10,11 +10,6 @@ import (
 )
 
 func Test_Dashboard_CRUD(t *testing.T) {
-	var (
-		boardLinks []sdk.FoundBoard
-		err        error
-	)
-
 	shouldSkip(t)
 	ctx := context.Background()
 	client := getClient(t)
@@ -22,33 +17,56 @@ func Test_Dashboard_CRUD(t *testing.T) {
 	var board sdk.Board
 	raw, _ := ioutil.ReadFile("testdata/new-empty-dashboard-2.6.json")
 
-	if err = json.Unmarshal(raw, &board); err != nil {
+	err := json.Unmarshal(raw, &board)
+	if err != nil {
 		t.Fatal(err)
 	}
 
+	// Check dashboard deletion.
 	client.DeleteDashboard(ctx, board.UpdateSlug())
+
+	// Check setting dashboard.
 	params := sdk.SetDashboardParams{
 		FolderID:  sdk.DefaultFolderId,
 		Overwrite: false,
 	}
-	if _, err = client.SetDashboard(ctx, board, params); err != nil {
+	_, err = client.SetDashboard(ctx, board, params)
+	if err != nil {
 		t.Fatal(err)
 	}
 
-	if boardLinks, err = client.SearchDashboards(ctx, "", false); err != nil {
+	// Check regular search of dashboards.
+	searchBoardLinks, err := client.SearchDashboards(ctx, "", false)
+	if err != nil {
 		t.Fatal(err)
 	}
-
-	if boardLinks, err = client.SearchDashboardsInFolders(ctx, []uint{1, 2}, "", false); err != nil {
-		t.Fatal(err)
-	}
-
-	for _, link := range boardLinks {
+	for _, link := range searchBoardLinks {
+		// Check dashboard retrieval by UID.
 		_, _, err = client.GetDashboardByUID(ctx, link.UID)
 		if err != nil {
 			t.Fatal(err)
 		}
 
+		// Check dashboard retrieval by Slug.
+		_, _, err = client.GetDashboardBySlug(ctx, link.URI)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// Check searching dashboards inside folders.
+	searchInFolderBoardLinks, err := client.SearchDashboardsInFolders(ctx, []uint{0}, "", false)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, link := range searchInFolderBoardLinks {
+		// Check dashboard retrieval by UID.
+		_, _, err = client.GetDashboardByUID(ctx, link.UID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Check dashboard retrieval by Slug.
 		_, _, err = client.GetDashboardBySlug(ctx, link.URI)
 		if err != nil {
 			t.Fatal(err)
