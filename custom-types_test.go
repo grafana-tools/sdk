779c9e685ef6043c97789985d61f6fa7c84231a2
Xabier Larrakoetxea <me@slok.dev>
2020-09-17 17:19:41 +0200

    Create a new custom type that can handle an string or slice of strings
    Signed-off-by: Xabier Larrakoetxea <me@slok.dev>

--- custom-types_test.go
+++ custom-types_test.go
@@ -22,6 +22,7 @@ package sdk_test
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/grafana-tools/sdk"
@@ -55,5 +56,139 @@ func TestIntString_Marshal(t *testing.T) {
 
 	if !bytes.Equal(body, []byte(`100`)) {
 		t.Error("Marshalled IntString is not valid: expected '100', got:", string(body))
+	}
+}
+
+func TestStringSliceString_Unmarshal(t *testing.T) {
+	tests := map[string]struct {
+		raw    string
+		exp    sdk.StringSliceString
+		expErr bool
+	}{
+		"Having an nil should unmarshall correctly as invalid.": {
+			raw: `"null"`,
+			exp: sdk.StringSliceString{
+				Valid: false,
+			},
+		},
+
+		"Having a string should unmarshall correctly.": {
+			raw: `"this is a test"`,
+			exp: sdk.StringSliceString{
+				Value: []string{"this is a test"},
+				Valid: true,
+			},
+		},
+
+		"Having a empty array should unmarshall correctly.": {
+			raw: `[]`,
+			exp: sdk.StringSliceString{
+				Value: []string{},
+				Valid: true,
+			},
+		},
+
+		"Having a single value array should unmarshall correctly.": {
+			raw: `["this is a test"]`,
+			exp: sdk.StringSliceString{
+				Value: []string{"this is a test"},
+				Valid: true,
+			},
+		},
+
+		"Having a multiple value array should unmarshall correctly.": {
+			raw: `["this", "is", "a", "test"]`,
+			exp: sdk.StringSliceString{
+				Value: []string{"this", "is", "a", "test"},
+				Valid: true,
+			},
+		},
+
+		"Having a wrong value should fail.": {
+			raw:    `[`,
+			expErr: true,
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			var got sdk.StringSliceString
+
+			err := json.Unmarshal([]byte(test.raw), &got)
+
+			if test.expErr {
+				if err == nil {
+					t.Error("Expected error, got nil")
+				}
+			} else {
+				if err != nil {
+					t.Errorf("Got unexpected error: %s", err)
+				}
+
+				if test.exp.Valid != got.Valid {
+					t.Errorf("Valid field is not valid, expected %t; got: %t", test.exp.Valid, got.Valid)
+				}
+
+				if fmt.Sprintf("%#v", test.exp.Value) != fmt.Sprintf("%#v", got.Value) {
+					t.Errorf("Value field is not valid, expected %#v; got: %#v", test.exp.Value, got.Value)
+				}
+
+			}
+		})
+	}
+}
+
+func TestStringSliceString_Marshall(t *testing.T) {
+	tests := map[string]struct {
+		value  *sdk.StringSliceString
+		exp    string
+		expErr bool
+	}{
+		"Having a single value should unmarshall correctly.": {
+			value: &sdk.StringSliceString{
+				Value: []string{"this is a test"},
+				Valid: true,
+			},
+			exp: `["this is a test"]`,
+		},
+
+		"Having an invalid value should return null.": {
+			value: &sdk.StringSliceString{},
+			exp:   `"null"`,
+		},
+
+		"Having a empty array should unmarshall correctly.": {
+			value: &sdk.StringSliceString{
+				Value: []string{},
+				Valid: true,
+			},
+			exp: `[]`,
+		},
+
+		"Having a multiple value array should unmarshall correctly.": {
+			value: &sdk.StringSliceString{
+				Value: []string{"this", "is", "a", "test"},
+				Valid: true,
+			},
+			exp: `["this","is","a","test"]`,
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := json.Marshal(test.value)
+
+			if test.expErr {
+				if err == nil {
+					t.Error("Expected error, got nil")
+				}
+			} else {
+				if err != nil {
+					t.Errorf("Got unexpected error: %s", err)
+				}
+
+				if test.exp != string(got) {
+					t.Errorf("Marshaled value is invalid, expected %s; got: %s", test.exp, string(got))
+				}
+			}
+		})
 	}
 }
