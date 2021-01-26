c1609780edf4d1de1b903887ef3ceae0cc845cd3
Giedrius Statkevičius <giedriuswork@gmail.com>
2021-01-26 22:09:00 +0200

    board: add initial visual test for singlestat panels

--- helpers_test.go
+++ helpers_test.go
@@ -1,19 +1,37 @@
 package sdk_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"testing"
 
 	"github.com/grafana-tools/sdk"
 )
 
-func getClient(t *testing.T) *sdk.Client {
+func getDebugURL(t *testing.T) string {
 	t.Helper()
 
-	addr := "http://localhost:3000"
-	user := "admin"
-	pass := "admin"
+	resp, err := http.Get("http://localhost:9222/json/version")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var result map[string]interface{}
+
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		t.Fatal(err)
+	}
+	return result["webSocketDebuggerUrl"].(string)
+}
+
+func getFullUrl(t *testing.T) (addr, user, pass string) {
+	t.Helper()
+
+	addr = "http://localhost:3000"
+	user = "admin"
+	pass = "admin"
 
 	if a := os.Getenv("GRAFANA_ADDR"); a != "" {
 		addr = a
@@ -24,6 +42,13 @@ func getClient(t *testing.T) *sdk.Client {
 	if p := os.Getenv("GRAFANA_PASS"); p != "" {
 		pass = p
 	}
+
+	return
+}
+
+func getClient(t *testing.T) *sdk.Client {
+	t.Helper()
+	addr, user, pass := getFullUrl(t)
 
 	return sdk.NewClient(addr, fmt.Sprintf("%s:%s", user, pass), sdk.DefaultHTTPClient)
 }
