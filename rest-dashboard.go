56218e48f2e4438741c0b77b32c57e93befc1872
Vincent Miszczak <vincent.miszczak@teads.com>
2022-02-16 12:39:59 +0100

    chore: add test and update style

--- rest-dashboard.go
+++ rest-dashboard.go
@@ -118,29 +118,25 @@ func (r *Client) GetDashboardBySlug(ctx context.Context, slug string) (Board, Bo
 
 // DashboardVersion represents a response from /api/dashboards/id/:dashboardId/versions API
 type DashboardVersion struct {
-	ID            int       `json:"id"`
-	DashboardID   int       `json:"dashboardId"`
-	ParentVersion int       `json:"parentVersion"`
-	RestoredFrom  int       `json:"restoredFrom"`
-	Version       int       `json:"version"`
+	ID            uint      `json:"id"`
+	DashboardID   uint      `json:"dashboardId"`
+	ParentVersion uint      `json:"parentVersion"`
+	RestoredFrom  uint      `json:"restoredFrom"`
+	Version       uint      `json:"version"`
 	Created       time.Time `json:"created"`
 	CreatedBy     string    `json:"createdBy"`
 	Message       string    `json:"message"`
 }
 
-// GetDashboardVersionsByID reflects /api/dashboards/id/:dashboardId/versions API call
-func (r *Client) GetDashboardVersionsByID(ctx context.Context, id uint, start, limit int) ([]DashboardVersion, error) {
+// GetDashboardVersionsByDashboardID reflects /api/dashboards/id/:dashboardId/versions API call
+func (r *Client) GetDashboardVersionsByDashboardID(ctx context.Context, id uint, params ...QueryParam) ([]DashboardVersion, error) {
 	var (
-		raw    []byte
-		code   int
-		err    error
-		params = url.Values{
-			"start": []string{strconv.Itoa(start)},
-			"limit": []string{strconv.Itoa(limit)},
-		}
+		raw  []byte
+		code int
+		err  error
 	)
 
-	if raw, code, err = r.get(ctx, fmt.Sprintf("api/dashboards/id/%d/versions", id), params); err != nil {
+	if raw, code, err = r.get(ctx, fmt.Sprintf("api/dashboards/id/%d/versions", id), queryParams(params...)); err != nil {
 		return nil, err
 	}
 	if code != 200 {
@@ -432,7 +428,33 @@ type (
 	SearchParam func(*url.Values)
 	// SearchParamType is a type accepted by SearchType func.
 	SearchParamType string
+	// QueryParam is a type for specifying arbitrary API parameters
+	QueryParam func(*url.Values)
 )
+
+// queryParams returns url.Values built from multiple QueryParam
+func queryParams(params ...QueryParam) url.Values {
+	u := url.URL{}
+	q := u.Query()
+	for _, p := range params {
+		p(&q)
+	}
+	return q
+}
+
+// QueryParamStart sets `start` parameter
+func QueryParamStart(start uint) QueryParam {
+	return func(v *url.Values) {
+		v.Set("start", strconv.Itoa(int(start)))
+	}
+}
+
+// QueryParamLimit sets `limit` parameter
+func QueryParamLimit(limit uint) QueryParam {
+	return func(v *url.Values) {
+		v.Set("limit", strconv.Itoa(int(limit)))
+	}
+}
 
 // Search entities to be used with SearchType().
 const (
