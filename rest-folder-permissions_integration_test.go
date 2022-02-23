61aa9d0fd02cdfd1aa67791892d5aa9b72adc6c5
Aleksander Aleksic <aleksander.aleksic@airthings.com>
2022-02-23 07:31:28 +0100

    Added folder permission REST support

---
+++ rest-folder-permissions_integration_test.go
@@ -0,0 +1,63 @@
+package sdk_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/grafana-tools/sdk"
+)
+
+func Test_FolderPermissions(t *testing.T) {
+	shouldSkip(t)
+
+	client := getClient(t)
+	ctx := context.Background()
+
+	var f = sdk.Folder{
+		Title: "test-permissions-folder",
+	}
+	var err error
+
+	folder, err := client.CreateFolder(ctx, f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.DeleteFolderByUID(ctx, folder.UID)
+
+	permissions, err := client.GetFolderPermissions(ctx, folder.UID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defaultPermissionCount := len(permissions)
+	if defaultPermissionCount == 0 {
+		t.Fatalf("Expected default permission count to be zero but was %d", defaultPermissionCount)
+	}
+
+	actualUser, err := client.GetActualUser(ctx)
+	if err != nil {
+		t.Fatalf("failed to get actual user: %s", err.Error())
+	}
+
+	var updatePermissions []sdk.UpdateFolderPermissionRequest
+	updatePermissions = append(updatePermissions, sdk.MapFolderPermissionsToUpdateRequest(permissions)...)
+	updatePermissions = append(updatePermissions, sdk.UpdateFolderPermissionRequest{
+		Permission: sdk.PermissionAdmin,
+		UserId: actualUser.ID,
+	})
+
+	_ , err = client.UpdateFolderPermissions(ctx, folder.UID,
+		updatePermissions...,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	updatedPermissions, err := client.GetFolderPermissions(ctx, folder.UID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	updatedPermissionCount := len(updatedPermissions)
+	if (defaultPermissionCount + 1) != updatedPermissionCount {
+		t.Fatal("Expected permissions count to increase by 1")
+	}
+}
